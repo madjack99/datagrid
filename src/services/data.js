c4088d0a95d6303c0f35792883bbd78da29b2ae2
madjack <kavanokami@gmail.com>
2020-03-04 18:18:11 +0400

    fix: age in data

--- src/services/data.js
+++ src/services/data.js
@@ -1,20 +1,12 @@
 import faker from 'faker';
 
-const getAge = () => {
-  let age = 0;
-  while (age < 18) {
-    age = Math.floor(Math.random() * 100);
-  }
-  return age;
-};
-
 const getFakeData = () => {
   faker.seed(1);
   const fakeData = [];
   for (let i = 0; i < 1001; i++) {
     const id = i;
     const name = faker.name.findName();
-    const age = getAge();
+    const age = faker.random.number({ min: 18, max: 100 });
     const job = faker.name.jobTitle();
     const married = faker.random.boolean() ? 'Yes' : 'No';
     const salary = faker.random.number();
