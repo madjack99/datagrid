29539974793a213bd2385e0e272dc742ff48cb27
madjack <kavanokami@gmail.com>
2020-03-08 17:29:45 +0400

    feat: add reverse sorting

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,14 @@ import getFakeData from '../services/data';
 
 const initialState = {
   personList: getFakeData(),
+  previouslySortedBy: null,
 };
 
 const sortByCategory = (state, category) => {
+  if (state.previouslySortedBy === category) {
+    return state.personList.reverse();
+  }
+
   return state.personList.sort((a, b) => {
     if (a[category] < b[category]) return -1;
     else if (a[category] > b[category]) return 1;
@@ -17,10 +22,12 @@ const reducer = (state = initialState, action) => {
     case 'FETCH_INITIAL_LIST':
       return {
         personList: [...initialState.personList],
+        previouslySortedBy: null,
       };
     case 'SORT_BY':
       return {
         personList: [...sortByCategory(state, action.payload)],
+        previouslySortedBy: action.payload,
       };
     default:
       return state;
