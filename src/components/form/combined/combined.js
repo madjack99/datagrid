ade54da0e15cb6f6c51a2e9893769ddea88e5f76
madjack <kavanokami@gmail.com>
2020-03-10 14:17:43 +0400

    feat: add multiple state selection

--- src/components/form/combined/combined.js
+++ src/components/form/combined/combined.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import FormText from '../text';
 import FormRadio from '../radio';
+import FormMultipleSelection from '../multiple-selection';
 
 function FormCombined() {
   return (
@@ -9,6 +10,7 @@ function FormCombined() {
       Form
       <FormText />
       <FormRadio />
+      <FormMultipleSelection />
     </div>
   );
 }
