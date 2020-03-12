ee8354f345e1bb3c3654ffef74fbc6292bde1864
madjack <kavanokami@gmail.com>
2020-03-12 14:41:13 +0400

    feat: add download csv file logic

--- src/components/csv-file/csv-file.js
+++ src/components/csv-file/csv-file.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-function CsvFile() {
-  return <div>csv</div>;
+function CsvFile({ personList }) {
+  const downloadCsv = () => {
+    let csv = 'ID,NAME,AGE,MARRIED,STATE,COMPANY,JOB,SALARY\n';
+    personList.forEach((item, index) => {
+      const itemValuesList = Object.values(item);
+      console.log(itemValuesList);
+      itemValuesList[0] = index;
+      itemValuesList[5] = itemValuesList[5].replace(/,/, ' ');
+      itemValuesList[6] = itemValuesList[6].replace(/,/, ' ');
+      csv += itemValuesList.join(',');
+      csv += '\n';
+    });
+
+    let hiddenElement = document.createElement('a');
+    hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+    hiddenElement.target = '_blank';
+    hiddenElement.download = 'people.csv';
+    hiddenElement.click();
+  };
+
+  return (
+    <div>
+      <button onClick={downloadCsv}>Download CSV file</button>
+    </div>
+  );
 }
 
-export default CsvFile;
+const mapStateToProps = ({ personList }) => ({
+  personList,
+});
+
+export default connect(mapStateToProps)(CsvFile);
