7d43ddb03b5626bdafff57d8037875536e15e79b
madjack <kavanokami@gmail.com>
2020-03-14 13:21:42 +0400

    feat: add redux devtools

--- src/store.js
+++ src/store.js
@@ -1,7 +1,10 @@
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const store = createStore(reducer, composeWithDevTools());
+
+// const store = createStore(reducer);
 
 export default store;
